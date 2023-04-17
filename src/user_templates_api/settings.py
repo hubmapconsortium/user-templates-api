d195d056126f7876c08194256dff4803865c1f6a
Juan Puerto <=>
2023-04-17 14:55:51 -0400

    General: Add corsheaders as requirement and allow CORS from all origins

--- src/user_templates_api/settings.py
+++ src/user_templates_api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CORS_ALLOW_ALL_ORIGINS = True
