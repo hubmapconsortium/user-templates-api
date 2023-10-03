8c6e7bec4f49881f66c3069fd9b3fe0ae09c0a28
thomcsmits <thomcsmits@outlook.com>
2023-10-03 07:43:40 -0400

    fix newline lint

--- src/user_templates_api/templates/jupyter_lab/templates/celltypes_snrnaseq/render.py
+++ src/user_templates_api/templates/jupyter_lab/templates/celltypes_snrnaseq/render.py
@@ -4,11 +4,3 @@ from user_templates_api.templates.jupyter_lab.render import JupyterLabRender
 class JupyterLabExampleJSONRender(JupyterLabRender):
     def __init__(self):
         pass
-
-# class JupyterLabAPITutorialRender(JupyterLabRender):
-#     def python_generate_template_data(self, data):
-#         uuids = data["body"]["uuids"]
-#         util_client = get_client(data["group_token"])
-
-#         cells = jl_utils._get_cells("template.txt", uuids, util_client)
-#         return cells
