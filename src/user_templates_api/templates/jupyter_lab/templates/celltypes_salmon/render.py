b18326153f990c06bac29270bdc93158d1b6e2bf
thomcsmits <thomcsmits@outlook.com>
2023-11-13 12:39:19 -0500

    add celltype visualization template in jinja

--- src/user_templates_api/templates/jupyter_lab/templates/celltypes_salmon/render.py
+++ src/user_templates_api/templates/jupyter_lab/templates/celltypes_salmon/render.py
@@ -1,6 +1,6 @@
 from user_templates_api.templates.jupyter_lab.render import JupyterLabRender
 
 
-class JupyterLabExampleJSONRender(JupyterLabRender):
+class JupyterLabExampleJinjaRender(JupyterLabRender):
     def __init__(self):
         pass
