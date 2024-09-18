595167d495f86c7b0528c8115e37c092da38e2f7
thomcsmits <thomcsmits@outlook.com>
2024-09-18 18:00:07 -0400

    add script + github action to check if all tags are included in src/tags.json

---
+++ src/check_tags.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+# read in the existing tags in tags.json
+with open("./src/tags.json") as file:
+    tags_dict = json.load(file)
+
+tags = list(tags_dict.keys())
+tags.sort()
+
+# retrieve all tags used in templates
+templates_path = "./src/user_templates_api/templates/jupyter_lab/templates/"
+templates = os.listdir(templates_path)
+
+template_tags = []
+for template in templates:
+    with open(templates_path + template + "/metadata.json") as file:
+        metadata = json.load(file)
+        if not metadata["is_hidden"]:
+            template_tags.extend(metadata["tags"])
+
+template_tags = list(set(template_tags))
+template_tags.sort()
+
+# check if all tags used in templates are part of tags.json
+missing_tags = [tag for tag in template_tags if tag not in tags]
+
+if len(missing_tags) > 0:
+    raise ValueError(
+        f"All tags in templates should exist in tags.json. The following tags are missing in tags.json: {missing_tags}"
+    )
