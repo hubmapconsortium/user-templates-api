b262473654d1fc21192867ba0d55d268a68da54b
srisharanya Injarapu <injarapu.s@ufl.edu>
2026-04-16 17:13:38 -0400

    Clean up blank lines in render.py
    Removed unnecessary blank lines in the render.py file.

--- src/user_templates_api/templates/jupyter_lab/templates/FUSION_tutorial/render.py
+++ src/user_templates_api/templates/jupyter_lab/templates/FUSION_tutorial/render.py
@@ -4,7 +4,3 @@ from user_templates_api.templates.jupyter_lab.render import JupyterLabRender
 class JupyterLabExampleJinjaRender(JupyterLabRender):
     def __init__(self):
         pass
-
-
-
-        
