05af2f6d613b319e957a455f6fd25c584b4fadfa
Srisharanya Injarapu <injarapu.s@ufl.edu>
2026-04-16 17:01:16 -0400

    made changes to clear cl issues.

--- src/user_templates_api/templates/jupyter_lab/templates/FUSION_tutorial/render.py
+++ src/user_templates_api/templates/jupyter_lab/templates/FUSION_tutorial/render.py
@@ -4,3 +4,7 @@ from user_templates_api.templates.jupyter_lab.render import JupyterLabRender
 class JupyterLabExampleJinjaRender(JupyterLabRender):
     def __init__(self):
         pass
+
+
+
+        
